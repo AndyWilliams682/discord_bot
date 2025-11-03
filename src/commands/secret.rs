b483552d81531751ea04d658028e3b4c9a6ae2ff
Andy Williams <andywilliams682@gmail.com>
2025-11-03 15:15:09 -0800

    Update secret.rs

--- src/commands/secret.rs
+++ src/commands/secret.rs
@@ -1,10 +1,22 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
 
-pub fn run(_options: &[CommandDataOption]) -> String {
-    "Hello secret Santa gamers, I have been informed that SOMEONE'S deck will arrive at their address within the week! HOWEVER, I have also been informed that the recipient should not open this package until a SECOND package arrives. HOW MYSTERIOUS".to_string()
+pub fn run(options: &[CommandDataOption]) -> String {
+    "Hey this command worked".to_string()
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("secret").description("Shhh, no telling")
+    command.name("secret").description("A secret message command").create_option(|option| {
+        option
+            .name("id")
+            .description("The user to message via the bot")
+            .kind(CommandOptionType::User)
+            .required(true)
+    }).create_option(|option| {
+        option
+            .name("message")
+            .description("The message to send")
+            .kind(CommandOptionType::String)
+            .required(true)
+    })
 }
