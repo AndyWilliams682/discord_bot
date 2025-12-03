9a35bde0af3de1d7e52fea95312f640fb20bf6ee
Andy Williams <andywilliams682@gmail.com>
2025-12-03 05:10:36 +0000

    Added more functionality to the database struct

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,11 @@
+use async_trait::async_trait;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 use serenity::prelude::TypeMapKey;
 use std::sync::Arc;
+
+use crate::commands::gotd::{InsertGif, SelectRandomGif};
 
 pub type DbPool = Pool<SqliteConnectionManager>;
 
@@ -14,4 +18,98 @@ impl TypeMapKey for DbPoolWrapper {
 pub fn establish_connection(db_path: &str) -> DbPool {
     let manager = SqliteConnectionManager::file(db_path);
     Pool::new(manager).expect("Failed to create pool.")
+}
+
+#[async_trait]
+pub trait InsertUser: Send + Sync {
+    async fn insert_user(&self, user_id: u64, username: String) -> Result<(), String>;
+}
+
+#[derive(Clone)]
+pub struct BotDatabase {
+    pool: DbPool,
+}
+
+impl BotDatabase {
+    pub fn new(pool: DbPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl InsertUser for BotDatabase {
+    async fn insert_user(&self, user_id: u64, username: String) -> Result<(), String> {
+        let pool_clone = self.pool.clone();
+        let conn = pool_clone.get().map_err(|e| e.to_string())?;
+        conn.execute(
+            "
+            INSERT OR IGNORE INTO users (user_id, username)
+            VALUES (?1, ?2);
+        ",
+            params![user_id, username],
+        )
+        .map_err(|e| e.to_string())?;
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl InsertGif for BotDatabase {
+    async fn insert_gif(&self, user_id: u64, username: String, url: String) -> Result<(), String> {
+        self.insert_user(user_id, username.clone()).await?;
+
+        let pool_clone = self.pool.clone();
+        tokio::task::spawn_blocking(move || {
+            let conn = pool_clone.get().map_err(|e| e.to_string())?;
+
+            conn.execute(
+                "
+                INSERT INTO gifs (submitted_by, url, posts)
+                VALUES (?1, ?2, 0);
+            ",
+                params![user_id, url],
+            )
+            .map_err(|e| e.to_string())?;
+            Ok(())
+        })
+        .await
+        .map_err(|e| e.to_string())?
+    }
+}
+
+#[async_trait]
+impl SelectRandomGif for BotDatabase {
+    async fn select_random_gif(&self) -> Result<(u64, String), String> {
+        let pool_clone = self.pool.clone();
+        tokio::task::spawn_blocking(move || {
+            let conn = pool_clone.get().map_err(|e| e.to_string())?;
+
+            let gif_stmt = "
+            SELECT submitted_by, url
+            FROM gifs
+            WHERE gifs.posts = (SELECT MIN(posts) FROM gifs)
+            ORDER BY RANDOM()
+            LIMIT 1;
+        ";
+
+            conn.query_row(gif_stmt, params![], |row| {
+                let gif_submitter: u64 = row.get(0)?;
+                let gif_url: String = row.get(1)?;
+
+                conn.execute(
+                    "
+                UPDATE gifs
+                SET posts = posts + 1
+                WHERE url = ?1;
+            ",
+                    params![gif_url.clone()],
+                )?;
+
+                Ok((gif_submitter, gif_url))
+            })
+            .map_err(|e| e.to_string())
+        })
+        .await
+        .map_err(|e| e.to_string())?
+    }
 }
